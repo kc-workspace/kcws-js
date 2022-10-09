bb206fc3c9a526238a8a4a1a79f9b37b20b9bba3
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-10-10 00:32:34 +0700

    chore: test commit-hooks [4]

--- packages/random/src/index.ts
+++ packages/random/src/index.ts
@@ -4,6 +4,6 @@ export const hello = (name: string): string => {
   return `hello ${name}`;
 };
 
-export const hi = (name: string): string => {
-  return `hi ${name}`;
+export const hi = (name: string) => {
+  return `hi ${name}`
 };
