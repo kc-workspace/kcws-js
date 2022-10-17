334d9ab136082aec4af8f6b3e7bd4db06473ec2e
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-10-17 21:51:31 +0700

    feat: add hooks:docs to git commit hooks to generate api-docs when developer create pr

---
+++ common/scripts/generate-docs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Support only yaml and markdown
+outtype="markdown"
+# Must be the same location as api-extractor docModel output
+# @kcws/node-rig/profiles/default/config/api-extractor.json
+input="common/temp/api"
+# Documentation directory
+output="docs"
+
+api-documenter "$outtype" \
+  --input-folder "$input" \
+  --output-folder "$output"
+
+unset outtype input output
