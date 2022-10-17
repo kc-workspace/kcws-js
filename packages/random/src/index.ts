3d5460049ee2a07fa46a1158f0138537a24fd0f1
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-10-17 21:56:57 +0700

    fix docs error

--- packages/random/src/index.ts
+++ packages/random/src/index.ts
@@ -1,5 +1,13 @@
 console.log("hello world");
 
+/**
+ * say hello
+ *
+ * @param name - input name
+ * @returns hello message
+ *
+ * @alpha
+ */
 export const hello = (name: string): string => {
   return `hello ${name}`;
 };
