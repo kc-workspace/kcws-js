d7133d3505beed6ba4ad4524140b5f2ddc2671fd
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-10-22 00:31:34 +0700

    fix: interactive doesn't work on while loop

--- common/autoinstallers/rush-dependencies-updater/index.sh
+++ common/autoinstallers/rush-dependencies-updater/index.sh
@@ -3,8 +3,7 @@
 root="$PWD"
 autoinstallers="$(dirname "$0")"
 
-cmd_name="ncu"
-config=".ncurc.yml"
+cmd_name="${COMMAND_NAME:-ncu}"
 
 if ! test -f "$root/rush.json"; then
   echo "This script must be run from the root of a Rush monorepo." >&2
@@ -21,17 +20,22 @@ if ! command -v "$cmd" >/dev/null 2>&1; then
   exit 1
 fi
 
+pkg="package.json"
+config=".ncurc.yml"
+tmp="$(mktemp)"
+
 find "$root" \
   -type f \
-  -name package.json \
+  -name "$pkg" \
   -and -not -path "*node_modules*" \
-  -and -not -path "*temp*" |
-  while IFS= read -r pjson; do
-    "$cmd" \
-      --packageFile "$pjson" \
-      --configFilePath "$root" \
-      --configFileName "$config" \
-      "$@"
-  done
+  -and -not -path "*temp*" >"$tmp"
+while IFS= read -r pjson <&3; do
+  "$cmd" \
+    --packageFile "$pjson" \
+    --configFilePath "$root" \
+    --configFileName "$config" \
+    "$@"
+done 3<"$tmp"
 
-unset root autoinstallers cmd cmd_name pjson config
+rm "$tmp"
+unset tmp root autoinstallers cmd cmd_name pjson config
