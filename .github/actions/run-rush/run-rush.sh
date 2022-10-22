a599a6d20fbb392cfb0d44fc3adb1280b4589862
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-10-22 08:48:40 +0700

    feat(ci): add reuse github action for install-rush and run-rush

---
+++ .github/actions/run-rush/run-rush.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cmd="${COMMAND:?}"
+# shellcheck disable=SC2206
+args=($ARGS)
+
+__shell=""
+__cmd="rush"
+
+if ! command -v "$__cmd" >/dev/null 2>&1; then
+  __shell="node"
+  __cmd="common/scripts/install-run-rush.js"
+fi
+
+if test -n "$__shell"; then
+  echo "Use install-run-rush.sh"
+  "$__shell" "$__cmd" "$cmd" "${args[@]}"
+else
+  echo "Use rush"
+  "$__cmd" "$cmd" "${args[@]}"
+fi
+
+unset cmd args __shell __cmd
