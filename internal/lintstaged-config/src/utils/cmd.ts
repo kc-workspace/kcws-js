088d2c1ec1292d2c55b9d661644c61d8d102492c
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-11-04 17:10:59 +0700

    chore: improve security concern

--- internal/lintstaged-config/src/utils/cmd.ts
+++ internal/lintstaged-config/src/utils/cmd.ts
@@ -1,4 +1,4 @@
-import { execSync } from "child_process";
+import { execSync, spawnSync } from "child_process";
 
 /**
  * Find command on the system path, and return the absolute path to the command;
@@ -11,11 +11,7 @@ import { execSync } from "child_process";
  */
 export const getCommand = (name: string): string => {
   const checking = ["command", "-v", name].join(" ");
-  try {
-    return execSync(checking, { encoding: "utf-8" });
-  } catch (e: unknown) {
-    // If we cannot resolve command name,
-    // just return them that let terminal handle an error
-    return name;
-  }
+  const output = spawnSync(checking, { shell: false, encoding: "utf8" });
+  if (output.status === 0) return output.stdout;
+  else return name;
 };
