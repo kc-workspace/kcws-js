fd9a53c811a31434d5e6d309105d5adc435763fe
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-11-04 17:22:45 +0700

    perf: ensure input command name is safe

--- internal/lintstaged-config/src/utils/cmd.ts
+++ internal/lintstaged-config/src/utils/cmd.ts
@@ -10,8 +10,10 @@ import { execSync, spawnSync } from "child_process";
  * @beta
  */
 export const getCommand = (name: string): string => {
-  const checking = ["command", "-v", name].join(" ");
-  const output = spawnSync(checking, { shell: false, encoding: "utf8" });
+  // Remove all space from input
+  const _name = name.replaceAll(" ", "");
+  const checking = ["command", "-v", _name].join(" ");
+  const output = spawnSync(checking, { shell: true, encoding: "utf8" });
   if (output.status === 0) return output.stdout;
   else return name;
 };
