a45f342467f931d1e2326de670394d2d1b59c51c
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-11-14 15:12:05 +0700

    feat: deploy docs from _docs instead and copy config over

--- common/autoinstallers/rush-api-documenter/index.sh
+++ common/autoinstallers/rush-api-documenter/index.sh
@@ -5,11 +5,16 @@ outtype="markdown"
 # Must be the same location as api-extractor docModel output
 # @kcws/node-rig/profiles/default/config/api-extractor.json
 input="common/temp/api"
+# Temporarily documentation directory
+temp_output="_docs"
 # Documentation directory
 output="docs"
 
 api-documenter "$outtype" \
   --input-folder "$input" \
-  --output-folder "$output"
+  --output-folder "$temp_output"
 
-unset outtype input output
+# Copy all configuration to temp_output
+cp -r "$output/" "$temp_output"
+
+unset outtype input temp_output output
