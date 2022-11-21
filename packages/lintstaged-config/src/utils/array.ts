dc6a34184968d531a1e318bd2acd994e48aa4f9c
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-11-21 17:05:20 +0700

    perf(utils): add isSame array and map checking

--- packages/lintstaged-config/src/utils/array.ts
+++ packages/lintstaged-config/src/utils/array.ts
@@ -3,3 +3,10 @@ export const toArray = <T>(s: T | Array<T> | undefined): Array<T> => {
   else if (s) return Array.of(s);
   else return [];
 };
+
+export const isSameArray = (
+  arr1: Array<unknown>,
+  arr2: Array<unknown>
+): boolean => {
+  return arr1.length === arr2.length && !arr1.some((v) => !arr2.includes(v));
+};
---
+++ packages/lintstaged-config/src/utils/map.ts
@@ -0,0 +1,12 @@
+export const isSameMap = <K, V>(
+  map1: Map<K, V>,
+  map2: Map<K, V>,
+  compareFn: (a: V, b: V) => boolean = (a, b) => a === b
+): boolean => {
+  return (
+    map1.size === map2.size &&
+    !Array.from(map1.entries()).some(([key, value]) => {
+      return !map2.has(key) || !compareFn(map2.get(key)!, value);
+    })
+  );
+};
