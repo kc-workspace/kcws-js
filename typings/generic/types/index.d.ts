053c72cfd2b960c6dcf080813e34a4542b34bbfe
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-11-25 21:11:18 +0700

    perf: add PartialK and RequiredK as global type for generic

--- typings/generic/types/index.d.ts
+++ typings/generic/types/index.d.ts
@@ -5,6 +5,28 @@ declare global {
 
   type Optional<T> = T | undefined | null;
   type Nullable<T> = Optional<T>; // alias
+
+  /**
+   * Same as Required, for only K keys.
+   * Anyelse, will be partial.
+   *
+   * @beta
+   */
+  type RequiredK<T, K extends keyof T = keyof T> = Required<
+    Pick<T, Extract<keyof T, K>>
+  > &
+    Partial<Omit<T, K>>;
+
+  /**
+   * Same as Partial, for only K keys.
+   * Anyelse, will be required.
+   *
+   * @beta
+   */
+  type PartialK<T, K extends keyof T = keyof T> = Partial<
+    Pick<T, Extract<keyof T, K>>
+  > &
+    Required<Omit<T, K>>;
 }
 
 export {};
--- typings/generic/types/test.ts
+++ typings/generic/types/test.ts
@@ -11,3 +11,20 @@ const b3: WithUndefined<string> = null;
 const c1: Optional<boolean> = false;
 const c2: Optional<boolean> = null;
 const c3: Optional<boolean> = undefined;
+
+interface Test {
+  a: number;
+  b: string;
+  c: boolean;
+  d: null;
+  e: object;
+  f: unknown;
+}
+
+const d1: RequiredK<Test, "a"> = { a: 123 };
+// @ts-expect-error
+const d2: RequiredK<Test, "a"> = {};
+
+// @ts-expect-error
+const e1: PartialK<Test, "b" | "c" | "d" | "e" | "f"> = {};
+const e2: PartialK<Test, "a"> = { b: "", c: false, d: null, e: {}, f: "" };
