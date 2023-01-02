57c7ec4d70fbea36d52fa91537d00a7c41917782
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-01-03 01:51:57 +0700

    perf(config): improve output package and ci/cd (#53)
    * perf(config): emit commonjs as default '.js', emit .mjs on same directory
    
    * perf: add current node version (19) to test
    
    * fix(lint): separate too long comment to multiple line
    
    * chore(config): enabled node 19 for testing only
    
    * perf(script): args on run-rush script is optional
    
    * chore(ci): separate main testing from alternative node version
    
    * chore(ci): add mergify to newest ci tasks
    
    * chore(ci): use git clone depth default value to reduce time to clone

--- .github/actions/run-rush/run-rush.sh
+++ .github/actions/run-rush/run-rush.sh
@@ -5,6 +5,14 @@ shift 1
 
 __shell=""
 __cmd="rush"
+__args=()
+
+for arg in "$@"; do
+  # add argument if and only if it isn't a empty string
+  if test -n "$arg"; then
+    __args+=("$arg")
+  fi
+done
 
 if ! command -v "$__cmd" >/dev/null 2>&1; then
   __shell="node"
@@ -14,11 +22,11 @@ fi
 if test -n "$__shell"; then
   echo "Use install-run-rush.sh"
   # shellcheck disable=SC2086
-  "$__shell" "$__cmd" "$cmd" "$@" || exit 1
+  "$__shell" "$__cmd" "$cmd" "${__args[@]}" || exit 1
 else
   echo "Use rush"
   # shellcheck disable=SC2086
-  "$__cmd" "$cmd" "$@" || exit 1
+  "$__cmd" "$cmd" "${__args[@]}" || exit 1
 fi
 
-unset cmd __shell __cmd
+unset cmd __shell __cmd __args
