c3b70926907b0691ab9931fb14f28a269f63038d
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-01-16 15:14:40 +0700

    perf(docs): improve document website and change theme

--- common/autoinstallers/rush-api-documenter/index-v2.sh
+++ common/autoinstallers/rush-api-documenter/index-v2.sh
@@ -1,32 +1,8 @@
 #!/usr/bin/env sh
 
-name="@kcws/js"
-output="_site"
-readme="none"
-plugin_name="@mxssfd/typedoc-theme"
-theme_name="my-theme"
-light_theme="solarized-light"
-dark_theme="solarized-dark"
+# current work directory
+CWD="$(dirname "$0")"
 
-# Verbose, Info, Warn, Error
-log_level="Warn"
+typedoc --options "$CWD/typedoc.json"
 
-typedoc \
-  --emit "docs" \
-  --out "$output" \
-  --logLevel "$log_level" \
-  --exclude "**/*+(index|.spec|.e2e).ts" \
-  --excludeNotDocumented \
-  --sort "visibility" --sort "static-first" \
-  --sort "enum-value-ascending" --sort "alphabetical" \
-  --plugin "$plugin_name" --theme "$theme_name" \
-  --lightHighlightTheme "$light_theme" \
-  --darkHighlightTheme "$dark_theme" \
-  --gaID "" --readme "$readme" \
-  --githubPages --cleanOutputDir --includeVersion \
-  --validation.invalidLink --validation.notExported \
-  --name "$name" \
-  --entryPointStrategy "packages" \
-  "packages/*" "typings/*"
-
-unset name output log_level readme light_theme dark_theme
+unset CWD
