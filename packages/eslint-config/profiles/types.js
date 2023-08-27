7ffea309e86e6074cab899b96f5def7321362031
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-08-27 12:04:58 +0700

    fix(dtlint): use @definitelytyped/eslint-plugin instead

--- packages/eslint-config/profiles/types.js
+++ packages/eslint-config/profiles/types.js
@@ -3,22 +3,12 @@ const base = require("./_base");
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: base.root,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@definitelytyped/eslint-plugin/all"],
+  plugins: ["@definitelytyped/eslint-plugin"],
   rules: {
     ...base.rules,
 
     // Mute this rules as it conflict with dtslint
     "@typescript-eslint/array-type": "off",
-
-    // Rules for types
-    // https://github.com/microsoft/DefinitelyTyped-tools/tree/master/packages/dtslint/src/rules
-    "dt-header": "error",
-    "export-just-namespace": "error",
-    "no-bad-reference": "error",
-    "no-dead-reference": "error",
-    "no-const-enum": "error",
-    "no-useless-files": "error",
-    "trim-file": "error",
   },
 };
