9d34a4d0c1c8d65f2023f0a4d06dd39383eb0e3b
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-12-26 20:02:00 +0700

    perf(eslint-config): add deprecation rules to support both javascript and typescript

--- packages/eslint-config/src/mixins/typescript.js
+++ packages/eslint-config/src/mixins/typescript.js
@@ -1,27 +1,35 @@
 const { defineConfig } = require("../utils/config");
 
 module.exports = defineConfig({
-  plugins: ["@typescript-eslint/eslint-plugin", "eslint-plugin-deprecation"],
   parserOptions: {
     project: "./tsconfig.json",
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  extends: [
-    // Only enabled if typescript is enabled
-    "plugin:eslint-plugin-deprecation/recommended",
-  ],
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
       excludedFiles: ["*.d.ts"],
-      extends: ["plugin:@typescript-eslint/recommended"],
-      parser: "@typescript-eslint/parser",
+      plugins: [
+        "@typescript-eslint/eslint-plugin",
+        "eslint-plugin-deprecation",
+      ],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:eslint-plugin-deprecation/recommended",
+      ],
       rules: {
         // Disable this when using with typescript
         // ref: https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unsupported-features/es-syntax.md
         "n/no-unsupported-features/es-syntax": "off",
       },
+    },
+    {
+      files: ["*.js", "*.jsx"],
+      extends: [
+        "plugin:@typescript-eslint/base",
+        "plugin:eslint-plugin-deprecation/recommended",
+      ],
     },
   ],
 });
