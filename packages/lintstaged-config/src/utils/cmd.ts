b8a48dbc8421c9889bbd81f258a12ba2ddd6425e
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-12-26 21:22:02 +0700

    fix(lintstaged-config): find command apis always throw error on regex

--- packages/lintstaged-config/src/utils/cmd.ts
+++ packages/lintstaged-config/src/utils/cmd.ts
@@ -11,7 +11,7 @@ import { spawnSync } from "node:child_process";
  */
 export const getCommand = (name: string): string => {
   // Remove spaces and symbols from input
-  const _name = name.replaceAll(/[ !#$%&*+@^]/, "");
+  const _name = name.replaceAll(/[ !#$%&*+@^]/g, "");
   const checking = ["command", "-v", _name].join(" ");
   const output = spawnSync(checking, { shell: true, encoding: "utf8" });
   return output.status === 0 ? output.stdout.trim() : name;
