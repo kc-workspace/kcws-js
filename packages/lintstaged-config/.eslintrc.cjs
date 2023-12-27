531a88a965f0b77ff6c47f4f474d5648046f4857
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-12-27 17:31:19 +0700

    fix(config): when logs missing on during executes it might throw `no such file or directory`

--- packages/lintstaged-config/.eslintrc.cjs
+++ packages/lintstaged-config/.eslintrc.cjs
@@ -2,6 +2,7 @@ const createConfig = require("@kcws/eslint-config");
 module.exports = createConfig({
   cwd: __dirname,
   profile: "node",
+  ecma: 2016,
   tsdoc: true,
   jest: true,
   rushstack: true,
