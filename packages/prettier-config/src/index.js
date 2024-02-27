fe7c0476b38681eb9b2ec553257f8217e7876054
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-02-27 22:43:57 +0700

    fix(prettier-config): invalid default function export and add data input

--- packages/prettier-config/src/index.js
+++ packages/prettier-config/src/index.js
@@ -1,16 +1,20 @@
 /**
  * create prettier config
  *
+ * @param {import('./types').Data} data - a config data
  * @returns {import("prettier").Config}
  */
-function createConfig() {
-  return {
+function createConfig(data) {
+  /** @type {import('./types').PrettierConfig} */
+  const base = {
     semi: true,
     trailingComma: "es5",
     singleQuote: false,
     arrowParens: "avoid",
     htmlWhitespaceSensitivity: "css",
   };
+
+  return { ...base, ...data.custom };
 }
 
-module.exports = createConfig();
+module.exports = createConfig;
---
+++ packages/prettier-config/src/types/index.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {import("prettier").Config} PrettierConfig
+ *
+ * @typedef {object} Data              - a data for generate prettier configs
+ * @property {PrettierConfig} [custom] - custom config
+ */
+
+module.exports = {};
