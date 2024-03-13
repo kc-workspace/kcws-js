1b943d534dcb8ae231e7a359ee66dfcfc6a8b977
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-03-13 11:04:57 +0700

    fix(random): update test description

--- packages/random/src/index.test.ts
+++ packages/random/src/index.test.ts
@@ -1,7 +1,7 @@
 import { RandomAlea, SeedTimestamp } from ".";
 
-describe("hello", () => {
-  it("should return hello world", () => {
+describe("Random alea", () => {
+  it("should return number within random range", () => {
     const rand = new RandomAlea(new SeedTimestamp());
     expect(rand.int({ min: 1, max: 10 })).toBeGreaterThan(0);
   });
