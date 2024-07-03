d8706ca0c504b21c33e97eeb858196ed06021084
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-07-03 18:01:25 +0700

    docs(github-actions): add more documents

--- packages/github-actions/src/builders/builder.type.ts
+++ packages/github-actions/src/builders/builder.type.ts
@@ -1,7 +1,15 @@
+/**
+ * All builder class should implement from this interface
+ * @public
+ */
 export interface Builder<V> {
   build(): V;
 }
 
+/**
+ * A type helper for get type of building data from builder object
+ * @public
+ */
 export type DataFromBuilder<B extends Builder<unknown>> = ReturnType<
   B["build"]
 >;
