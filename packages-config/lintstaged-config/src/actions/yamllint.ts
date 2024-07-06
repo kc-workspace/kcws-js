a405ff550e53debb13b4b1b657e7d07adab4d9a8
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-07-07 00:00:17 +0700

    fix(lintstaged-config): default config file should apply only if file existed

--- packages-config/lintstaged-config/src/actions/yamllint.ts
+++ packages-config/lintstaged-config/src/actions/yamllint.ts
@@ -1,5 +1,7 @@
 import type { IBaseActionOptions, BaseActionFn } from "./_base";
 
+import { existsSync } from "node:fs";
+
 import { getCommand } from "../utils/cmd";
 
 /**
@@ -32,12 +34,16 @@ export const DEFAULT_YAMLLINT_CONFIG: string = ".github/linters/.yamllint.yml";
  *
  * @beta
  */
-export const yamllint: BaseActionFn<IYamllintOptions> = (option) => {
+export const yamllint: BaseActionFn<IYamllintOptions> = option => {
   const args: Array<string> = [getCommand("yamllint")];
 
-  const configFile = option?.config ?? DEFAULT_YAMLLINT_CONFIG;
-  args.push("--config-file", configFile);
-
+  // If config option existed, always use option regardless of file existence
+  if (option?.config) {
+    args.push("--config-file", option?.config);
+    // Else check if default config is existed, then use the default config
+  } else if (existsSync(DEFAULT_YAMLLINT_CONFIG)) {
+    args.push("--config-file", DEFAULT_YAMLLINT_CONFIG);
+  }
   if (option?.strict ?? true) args.push("--strict");
 
   const files = option?.files ?? [];
